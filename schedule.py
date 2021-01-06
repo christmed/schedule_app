cbd58e33bb1598d26a9c055aea706113cd8ba2f6
christmed <medinacr96@gmail.com>
2021-01-05 19:46:17 -0600

    can now add activities to the schedule

--- schedule.py
+++ schedule.py
@@ -1,6 +1,7 @@
 import datetime
 import time
 import sys
+import re
 
 
 def today_greeter():
@@ -17,7 +18,7 @@ def print_instructions():
     instructions = "\nEnter 'q' at any time to quit."
     instructions += "\nEnter the start time and end time of the activity."
     instructions += "\n'Start time' must be earlier than the 'End time'."
-    instructions += "\nFor example, Start: 12:00 PM End: 4:30 PM\n"
+    instructions += "\nE.g. Start: 12:00 PM End: 04:30 PM\n"
     print(instructions)
 
 
@@ -55,13 +56,14 @@ def add_activity(start, end, activity, schedule):
     **activity: to be added to the schedule.
     **schedule: the current version of the schedule.
     """
-    # Get the starting hour.
+
+    # Get the starting hour key.
     for key, value in schedule.items():
         for hour in value:
             if hour.startswith(str(start)):
                 start = key
 
-    # Get the ending hour.
+    # Get the ending hour key.
     for key, value in schedule.items():
         for hour in value:
             if hour.endswith(str(end)):
@@ -73,8 +75,17 @@ def add_activity(start, end, activity, schedule):
         for hour in value:
             if key in range(index[0], index[1]):
                 schedule[key][hour] = activity
-
     return schedule
+
+
+def show_current_changes(schedule):
+    for value in c_schedule.values():
+        for hr, act in value.items():
+            if act != '':
+                print(f"{hr}\t\t{act}")
+            else:
+                pass
+
 
 def sim_update():
     """Simulates schedule update."""
@@ -82,6 +93,22 @@ def sim_update():
     time.sleep(1)
     print('Done\n')
 
+
+def check_format(pattern, str):
+    """Checks if pattern is a valid option.
+
+    **pattern:
+    **str:
+    """
+
+    matched = re.match(pattern, str)
+    is_match = bool(matched)
+
+    return is_match
+
+
+option_error = 'Please choose a valid option' \
+      '\nE.g Start: 07:00 AM End: 02:00 PM'
 
 name = input('Welcome to "The Daily Scheduler", What is your name? ')
 today_greeter()
@@ -93,14 +120,31 @@ print_schedule(c_schedule)
 
 while True:
     start = input('\nStart: ')
+    if start == 'q':
+        sys.exit()
+
+    # Checks that format matches the schedule hours format.
+    pattern = re.compile(r"[0-1][0-9]\W[0-3][0]\s[APM]")
+    matched_pattern = check_format(pattern, start)
+    if not matched_pattern:
+        print(option_error)
+        continue
+
     end = input('End: ')
+    if end == 'q':
+        sys.exit()
+    matched_pattern = check_format(pattern, start)
+    if not matched_pattern:
+        print(option_error)
+        continue
+
 
     # Adds activity to the schedule.
-    print(f"You selected {start} - {end}\n")
+    print(f"\nYou selected {start} - {end}")
     activity = input(("Which activity would you like "
-                      "to add to the schedule? "))
+                      "to add to the schedule?\n "))
+    c_schedule = add_activity(start, end, activity, c_schedule)
 
-    add_activity(start, end, activity, c_schedule)
-
-
-
+    # Shows only the changes made to the schedule.
+    sim_update()
+    show_current_changes(c_schedule)
